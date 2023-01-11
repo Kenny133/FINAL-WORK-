270108848be950355f64ccaff0980858b65b7352
Kenny133 <ttt.sabir@gmail.com>
2023-01-11 04:56:49 +0300

    work01

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class HelloWorld 
+{
+    static void Main() 
+    {
+                string[] A = new string[]{"Hello","2","World",":-)"};
+        foreach(string a in A)
+        Console.Write(a + " ");
+        int i = 0;
+        
+    }
+}
